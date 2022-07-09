7df87f130f764f5714651fc71b210024743d3baf
sritapam <pereyrapame@gmail.com>
2022-07-09 16:12:28 -0300

    SQL

---
+++ clase15/sql.js
@@ -0,0 +1,14 @@
+// base de datos mundo;
+
+// personas;
+// id | nombre | appellido  | ciudad
+//  1    pamela    pereyra       1
+//  2    Pedro     lopez         2
+
+
+
+// ciudades;
+
+// id | nombre | poblacion | area 
+//  1   cordoba   10000       20  
+//  2   bogota     2000        2000
